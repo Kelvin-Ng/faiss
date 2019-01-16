cfd6ae622ff74765cd2bda04828fa0d894495d28
kaihaoma <goheadmkh@gmail.com>
2019-01-16 11:37:44 +0800

    SimpleIMI implementation

--- gpu/impl/SimpleIMICut.cu
+++ gpu/impl/SimpleIMICut.cu
@@ -6,31 +6,30 @@ namespace faiss { namespace gpu {
 
 
 void runSimpleIMIcut(const Tensor<float, 3, true>& imiDistances,
-						   Tensor<int, 2, true>& imiUpperBounds,
-						   int squareLen;
-						   int totalLen;
-						   cudaStream_t stream) {
+                     Tensor<int, 2, true>& imiUpperBounds,
+                     int squareLen;
+                     int totalLen;
+                     cudaStream_t stream) {
 
-
-		runSimpleIMISelect(imiDistances,
-						   imiUpperBounds,
-						   squareLen,
-						   totalLen,
-						   cudaStream_t stream);
-	}
+     runSimpleIMISelect(imiDistances,
+                        imiUpperBounds,
+                        squareLen,
+                        totalLen,
+                        cudaStream_t stream);
+}
 
 #ifdef FAISS_USE_FLOAT16
 void runSimpleIMIcut(const Tensor<half, 3, true>& imiDistances,
-					 Tensor<int, 2, true>& imiUpperBounds,
-					 int squareLen;
-					 int totalLen;
-					 cudaStream_t stream) {
+                     Tensor<int, 2, true>& imiUpperBounds,
+                     int squareLen;
+                     int totalLen;
+                     cudaStream_t stream) {
 
-	runSimpleIMISelect(imiDistances,
-					   imiUpperBuounds,
-					   squareLen,
-					   totalLen,
-					   cudaStream_t stream);
+     runSimpleIMISelect(imiDistances,
+                        imiUpperBuounds,
+                        squareLen,
+                        totalLen,
+                        cudaStream_t stream);
 }
 
 #endif
--- gpu/impl/SimpleIMISelect.cu
+++ gpu/impl/SimpleIMISelect.cu
@@ -34,9 +34,9 @@ namespace faiss { namespace gpu {
 template <typename T, typename TVec, typename int64_t,
           int RowTileSize, bool NormLoop>
 __global__ void SimpleIMISelect(Tensor<TVec, 3, true, int64_t> input,
-                       Tensor<int, 2, true, int64_t> output
-					   int S,
-					   int T) {
+                                Tensor<int, 2, true, int64_t> output
+                                int S,
+                                int T) {
   extern __shared__ char smemByte1[]; // #warps * RowTileSize elements
   extern __shared__ char smemByte2[];
   T* smemMin = (T*) smemByte1;
@@ -60,21 +60,21 @@ __global__ void SimpleIMISelect(Tensor<TVec, 3, true, int64_t> input,
              col < T - 2 * S ; col += blockDim.x) {
           TVec val = abs(input[0][rowStart + row][col + S] - input[1][rowStart + row][col + S]);
           //rowNorm[0] = Math<T>::add(rowNorm[0], Math<TVec>::reduceAdd(val));
-		  rowId[0] = ( val < rowMin[0] ? col + S : rowId[0]);
-		  rowMin[0] = ( val < rowMin[0] ? val : rowMin[0]);
+          rowId[0] = ( val < rowMin[0] ? col + S : rowId[0]);
+          rowMin[0] = ( val < rowMin[0] ? val : rowMin[0]);
         }
       } else {
         TVec val = abs(input[0][rowStart + row][threadIdx.x + S] - input[1][rowStart + row][threadIdx.x + S]);
         //val = Math<TVec>::mul(val, val);
         //rowNorm[0] = Math<TVec>::reduceAdd(val);
-		rowId[0] = ( val < rowMin[0] ? col + S : rowId[0]);
-		rowMin[0] = ( val < rowMin[0] ? val : rowMin[0]);
+        rowId[0] = ( val < rowMin[0] ? col + S : rowId[0]);
+        rowMin[0] = ( val < rowMin[0] ? val : rowMin[0]);
       }
 
       //rowNorm[0] = warpReduceAllMin(rowNorm[0]);
-	  pair<T,int> res = warpReduceAllMin(rowMin[0],rowId[0]);
-	  rowMin[0] = res.first;
-	  rowId[0] = res.second;
+      pair<T,int> res = warpReduceAllMin(rowMin[0],rowId[0]);
+      rowMin[0] = res.first;
+      rowId[0] = res.second;
       if (laneId == 0) {
         smemMin[row * numWarps + warpId] = rowMin[0];
 		smemId[row * numWarps + warpId] = rowId[0];
@@ -95,17 +95,17 @@ __global__ void SimpleIMISelect(Tensor<TVec, 3, true, int64_t> input,
       }
 
       for (int64_t col = threadIdx.x;
-           col < T - 2 * S; col += blockDim.x) {
+        col < T - 2 * S; col += blockDim.x) {
 
 #pragma unroll
-        for (int row = 0; row < RowTileSize; ++row) {
-          tmp[row] = abs(input[0][rowStart + row][col + S]-input[1][rowStart+row][col + S]);
+      for (int row = 0; row < RowTileSize; ++row) {
+        tmp[row] = abs(input[0][rowStart + row][col + S]-input[1][rowStart+row][col + S]);
         }
 
 #pragma unroll
-        for (int row = 0; row < RowTileSize; ++row) {
-		  rowId[row] = (tmp[row] < rowMin[row] ? col + S : rowId[row]);
-		  rowMin[row] = (tmp[row] < rowMin[row] ? tmp[row] : rowMin[row]);
+      for (int row = 0; row < RowTileSize; ++row) {
+        rowId[row] = (tmp[row] < rowMin[row] ? col + S : rowId[row]);
+        rowMin[row] = (tmp[row] < rowMin[row] ? tmp[row] : rowMin[row]);
         }
       }
     } else {
@@ -120,7 +120,7 @@ __global__ void SimpleIMISelect(Tensor<TVec, 3, true, int64_t> input,
 #pragma unroll
       for (int row = 0; row < RowTileSize; ++row) {
         rowId[row] = (tmp[row] < rowMin[row] ? threadIdx.x + S : rowId[row]);
-		rowMin[row] = (tmp[row] < rowMin[row] ? tmp[row] : rowMin[row]);
+        rowMin[row] = (tmp[row] < rowMin[row] ? tmp[row] : rowMin[row]);
       }
     }
 
@@ -135,9 +135,9 @@ __global__ void SimpleIMISelect(Tensor<TVec, 3, true, int64_t> input,
 
     if (laneId == 0) {
 #pragma unroll
-      for (int row = 0; row < RowTileSize; ++row) {
-        smemMin[row * numWarps + warpId] = rowMin[row];
-		smemId[row * numWarps + warpId] = rowId[row];
+    for (int row = 0; row < RowTileSize; ++row) {
+      smemMin[row * numWarps + warpId] = rowMin[row];
+      smemId[row * numWarps + warpId] = rowId[row];
       }
     }
   }
@@ -149,42 +149,42 @@ __global__ void SimpleIMISelect(Tensor<TVec, 3, true, int64_t> input,
 #pragma unroll
     for (int row = 0; row < RowTileSize; ++row) {
       rowMin[row] = laneId < numWarps ? smemMin[row * numWarps + laneId] : 1e10;
-	  rowId[row] = laneId < numWarps ? smemId[row * numWarps + laneId] : Math<T>::zero();
+      rowId[row] = laneId < numWarps ? smemId[row * numWarps + laneId] : Math<T>::zero();
     }
 
 #pragma unroll
     for (int row = 0; row < RowTileSize; ++row) {
       pair<T, int> res = warpReduceAllMin(rowMin[row], rowId[row]);
-	  rowMin[row] = res.first;
-	  rowId[row] = res.second;
+      rowMin[row] = res.first;
+      rowId[row] = res.second;
     }
 
     // Write out answer
     if (laneId == 0) {
 #pragma unroll
-      for (int row = 0; row < RowTileSize; ++row) {
-        int outCol = rowStart + row;
-        if (lastRowTile) {
-		  if(outCol < output.getSize(1)) {
-		    output[0][outcol] = rowId[row];
-			output[1][outcol] = T - rowId[row];
+    for (int row = 0; row < RowTileSize; ++row) {
+      int outCol = rowStart + row;
+      if (lastRowTile) {
+         if(outCol < output.getSize(1)) {
+           output[0][outcol] = rowId[row];
+	   output[1][outcol] = T - rowId[row];
         }
       }
-	  else {
-	  output[0][outcol] = rowId[row];
-	  output[1][outcol] = T - rowId[row];
-	  	}
-	  }
+	 else {
+	 output[0][outcol] = rowId[row];
+	 output[1][outcol] = T - rowId[row];
+	}
+      }
     }
   }
 }
 
 template <typename T, typename TVec, typename int64_t>
 void runSimpleIMISelct(Tensor<T, 3, true, int64_t>& input,
-               Tensor<int, 2, true, int64_t>& output,
-               int S,
-			   int T,
-               cudaStream_t stream) {
+                       Tensor<int, 2, true, int64_t>& output,
+                       int S,
+                       int T,
+                       cudaStream_t stream) {
   FAISS_ASSERT(input.getSize(0) == output.getSize(0));
 
   int64_t maxThreads = (int64_t) getMaxThreadsCurrentDevice();
@@ -193,12 +193,12 @@ void runSimpleIMISelct(Tensor<T, 3, true, int64_t>& input,
 #define RUN_L2(TYPE_T, TYPE_TVEC, INPUT)                                \
   do {                                                                  \
     if (normLoop) {                                                     \
-        SimpleIMISelect<TYPE_T, TYPE_TVEC, int64_t, rowTileSize, true>  \
-          <<<grid, block, smem, stream>>>(INPUT, output, S, T);         \
+      SimpleIMISelect<TYPE_T, TYPE_TVEC, int64_t, rowTileSize, true>    \
+        <<<grid, block, smem, stream>>>(INPUT, output, S, T);           \
     } else {                                                            \
         SimpleIMISelect<TYPE_T, TYPE_TVEC, int64_t, rowTileSize, false> \
           <<<grid, block, smem, stream>>>(INPUT, output, S, T);         \
-	  }                                                                 \
+	  }                                                             \
   } while (0)                                                           \
 
   if (input.template canCastResize<TVec>()) {
@@ -236,10 +236,10 @@ void runSimpleIMISelct(Tensor<T, 3, true, int64_t>& input,
 }
 
 void runSimpleIMISelect(Tensor<float, 3, true>& input,
-               Tensor<int, 2, true>& output,
-               int S;
-			   int T;
-               cudaStream_t stream) {
+                        Tensor<int, 2, true>& output,
+                        int S;
+                        int T;
+                        cudaStream_t stream) {
   if (input.canUseIndexType<int>()) {
     runSimpleIMISelect<float, int, int>(input, output, S, T, stream);
   } else {
@@ -251,10 +251,10 @@ void runSimpleIMISelect(Tensor<float, 3, true>& input,
 
 #ifdef FAISS_USE_FLOAT16
 void runSimpleIMISelect(Tensor<half, 3, true>& input,
-               Tensor<int, 2, true>& output,
-               int S;
-			   int T;
-               cudaStream_t stream) {
+                        Tensor<int, 2, true>& output,
+                        int S;
+                        int T;
+                        cudaStream_t stream) {
   if (input.canUseIndexType<int>()) {
     runSimpleIMISelect<half, int, int>(input, output, S, T, stream);
   } else {
